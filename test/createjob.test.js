29dae5cda8a8806cc30c604c6cd08fd517c918b8
Chambers <bobhus394@gmail.com>
2023-08-01 16:46:13 +0800

    api updated & cancel model required

--- test/createjob.test.js
+++ test/createjob.test.js
@@ -14,10 +14,6 @@ describe('create a job', () => {
     await expect(prodiai.createJob({ prompt: '' })).rejects.toThrowError('Prompt is required!')
   })
 
-  it('it should get an error because model name provided was not accepted', async () => {
-    await expect(prodiai.createJob({ prompt: 'a dog', model: 'foo model' })).rejects.toThrowError('Model not accepted')
-  })
-
   it('it should resolve job by give prompt', async () => {
     await expect(prodiai.createJob({ prompt: 'a dog' })).resolves.toBeTypeOf('object')
   })
